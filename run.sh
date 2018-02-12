d9ab7cb241e5df8d600b5817f04c143ca9481d9e
Jason Antman <jason@jasonantman.com>
2018-02-12 18:15:42 -0500

    cleanup run.sh test script

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+function cleanup {
+  docker stop selfoss-test
+  docker rm selfoss-test
+}
+
+trap cleanup EXIT
 docker run --name selfoss-test -d -P jantman/docker-selfoss:latest
 docker inspect --format='{{.NetworkSettings.Ports}}' selfoss-test
 docker logs -f selfoss-test
